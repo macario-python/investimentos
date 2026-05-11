600ddaf2df67e6d90f232c94d20d5d1e16cd0983
macario-python <renato.macario16@gmail.com>
2026-05-11 15:14:19 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
-self.addEventListener('install', e =
+// AlertaInvest SW v5 - limpo
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => { 
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', e => { return; });
